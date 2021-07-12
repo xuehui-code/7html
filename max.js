8dd51115c8040b73cffb34f60e3c36d7f5c56afc
xuehui-code <74036099+xuehui-code@users.noreply.github.com>
2021-07-12 19:46:23 +0800

    Add files via upload

---
+++ max.js
@@ -0,0 +1,11 @@
+var a = prompt('请输入数据1');
+var b = prompt('请输入数据2');
+
+console.log('数据1是'+a);
+console.log('数据2是'+b);
+
+if(a>b){
+    console.log('最大值是'+a);
+}else{
+    console.log('最大值是'+b);
+}
